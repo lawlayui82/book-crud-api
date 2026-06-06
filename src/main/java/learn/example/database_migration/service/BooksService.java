e0cebabb3bd41de583abda695e262f1dc0a49752
lawlayui82 <lawlayui82@gmail.com>
2026-06-06 07:27:31 +0700

    wip: add books service, implement mapTo

---
+++ src/main/java/learn/example/database_migration/service/BooksService.java
@@ -0,0 +1,38 @@
+package learn.example.database_migration.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import learn.example.database_migration.api.dto.BooksRequestDTO;
+import learn.example.database_migration.api.dto.BooksResponseDTO;
+import learn.example.database_migration.entity.Book;
+
+@Service
+public class BooksService extends BaseService<Book, Long, BooksResponseDTO, BooksRequestDTO>{
+    public Book mapToEntity(BooksRequestDTO request) {
+        Book book = new Book(request.getId(), request.getTitle(), request.getDescription(), request.getAuthor(), request.getYear(), request.getPrice(), request.getStock());
+        return book;
+    }
+
+    public BooksResponseDTO mapToResponse(Book book) {
+        BooksResponseDTO bookResponse = BooksResponseDTO.builder() 
+        .id(book.getId())
+        .title(book.getTitle())
+        .author(book.getAuthor())
+        .description(book.getDescription())
+        .createdAt(book.getCreatedAt())
+        .updateAt(book.getUpdateAt())
+        .build();
+        return bookResponse;
+    }
+
+    public List<BooksResponseDTO> mapToResponse(List<Book> books) {
+        List<BooksResponseDTO> booksResponse = books.stream()
+            .map((book) -> new BooksResponseDTO(book.getId(), book.getCreatedAt(), book.getUpdateAt(), book.getTitle(), book.getDescription(), book.getAuthor(), book.getPrice(), book.getStock(), book.getYear()))
+            .collect(Collectors.toList());
+
+        return booksResponse;
+    }
+}

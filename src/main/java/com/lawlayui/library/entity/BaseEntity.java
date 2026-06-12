5c1f880ef663536e131a22a8951d7fa3c357b000
lawlayui82 <lawlayui82@gmail.com>
2026-06-12 12:56:44 +0700

    Refactor BaseEntity to use Hibernate annotations for timestamp management

--- src/main/java/com/lawlayui/library/entity/BaseEntity.java
+++ src/main/java/com/lawlayui/library/entity/BaseEntity.java
@@ -2,13 +2,14 @@ package com.lawlayui.library.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,20 +27,11 @@ public abstract class BaseEntity<ID> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ID id;
 
+    @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     @Column(name = "update_at")
     private LocalDateTime updateAt;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.updateAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updateAt = LocalDateTime.now();
-    }
 }
